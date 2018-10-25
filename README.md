cb961ad553de8fc2863b30a25d7c6144b3f0c051
alexmorley <alexander.morley@univ.ox.ac.uk>
2018-10-25 11:46:48 +0100

    Automate develop start up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # remix.science
+
+At the moment there are a few moving parts required to start developing. First, you will need to have kbucket installed, and have it's utilities available in your path. Then, if you use tmux, getting started should just require cloning the repository and running ./develop.sh. This will open a couple of screens, running each server in a different pane. 
---
+++ develop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+SESSION=remix
+
+tmux -2 new-session -d -s $SESSION
+
+# set up app window
+tmux new-window -t $SESSION:2 -n 'app'
+tmux split-window -h
+tmux select-pane -t 0
+tmux send-keys "cd app && npm run dev" C-m
+
+# set up kbucket moving parts window
+tmux new-window -t $SESSION:3 -n 'kbucket'
+tmux send-keys "mkdir -p ~/.scratch && \cp -Rf develop.scratch/* ~/.scratch/" C-m
+tmux split-window -h
+tmux split-window -h
+
+## kbucket hub pane
+tmux select-pane -t 0
+tmux send-keys "cd ~/.scratch/temporay1 && kbucket-hub --auto" C-m
+
+## lari hub pane
+tmux select-pane -t 1
+tmux send-keys "cd ~/.scratch/temporary2 && lari-hub --auto" C-m
+
+## lari host pane
+tmux select-pane -t 2
+tmux send-keys "cd ~/.scratch/temporary3 && lari-host --auto" C-m
+tmux select-layout even-horizontal
+
+tmux select-window -t $SESSION:3
+
+tmux -2 attach-session -t $SESSION
